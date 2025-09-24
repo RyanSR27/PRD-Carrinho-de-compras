0faa2087c1960844dd4d095d373d58e305fb5a4c
RyanSR27 <ryanrodrigues.sa@gmail.com>
2025-09-23 22:45:17 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-include_once('./class/Carrinho.php');
+include_once('./src/Carrinho.php');
 
 $carrinho = new Carrinho([
     ['id' => 1, 'nome' => 'Camiseta', 'preco' => 59.90, 'estoque' => 10],
@@ -12,40 +12,50 @@ $carrinho = new Carrinho([
  * Caso de uso 1: Adicionar item válido ao carrinho
  * Resultado: $err vazio, carrinho com produto adicionado
  */
+echo"Caso 1 - Adicionar item válido ao carrinho <br>";
 $err = $carrinho->adicionarItemAoCarrinho(1, 2);
-var_dump($carrinho->obterResumo());
+print $err . "<br><br>";
+//var_dump($carrinho->obterResumo());
 
 /**
  * Caso de uso 2: Adicionar item além do estoque
  * Resultado: $err com mensagem, produto não adicionou no carrinho
  */
-$err = $carrinho->adicionarItemAoCarrinho(3, 10);
-print $err;
+echo"Caso 2 - Adicionar item além do estoque <br>";
+$err = $carrinho->adicionarItemAoCarrinho(3, 10) . "<br>";
+print $err . "<br>";
 
 /**
  * Caso de uso adicional: Adicionar produto não existente
  * Resultado: $err com mensagem, produto não encontrado
  */
-$err = $carrinho->adicionarItemAoCarrinho(4, 1);
-print $err;
+echo"Caso de uso adicional - Adicionar produto não existente <br>";
+$err = $carrinho->adicionarItemAoCarrinho(4, 1) . "<br>";
+print $err  . "<br>";
 
 /**
  * Caso de uso adicional: Adicionar quantidade 1 do mesmo produto duas vezes
  * Resultado: Carrinho com quantidade 2
  */
-$carrinho->adicionarItemAoCarrinho(1,1);
-$carrinho->adicionarItemAoCarrinho(1,1);
-var_dump($carrinho->obterResumo());
+echo"Caso de uso adicional - Adicionar quantidade 1 do mesmo produto duas vezes <br>"; 
+$err = $carrinho->adicionarItemAoCarrinho(1,1);
+print $err . "<br>";
+$err = $carrinho->adicionarItemAoCarrinho(1,1);
+print $err . "<br><br>";
+//var_dump($carrinho->obterResumo()) . "<br><br>";
 
 /**
  * Caso de uso 3: Remover produto do carrinho
  * Antes de remover, é preciso adicionar primeiro
  * Resultado: Estoque restaurado, carrinho vazio
  */
+echo"Caso de uso 3 - Remover produto do carrinho <br>";
 $err = $carrinho->adicionarItemAoCarrinho(1,1);
-var_dump($carrinho->obterResumo()); // 1 produto no carrinho, estoque 9
+print $err . "<br>";
+//var_dump($carrinho->obterResumo()); // 1 produto no carrinho, estoque 9
 $err = $carrinho->removerItemDoCarrinho(1);
-var_dump($carrinho->obterResumo()); // carrinho vazio, estoque retorna pra 10
+print $err . "<br>";
+//var_dump($carrinho->obterResumo()) . "<br><br>"; // carrinho vazio, estoque retorna pra 10
 
 /**
  * Caso de uso 4: Aplicando cupom de desconto
@@ -53,12 +63,15 @@ var_dump($carrinho->obterResumo()); // carrinho vazio, estoque retorna pra 10
  * Resultado: Carrinho com subtotal mostrando o preço cheio, e o total mostrando preço com desconto.
  * Cenário adicional: remover o cupom deve voltar os totais
  */
+echo"Caso de uso 4 - Aplicando cupom de desconto <br>";
 $carrinho->adicionarItemAoCarrinho(1, 1);
 $carrinho->adicionarItemAoCarrinho(2, 1);
 $carrinho->adicionarItemAoCarrinho(3, 1);
 $err = $carrinho->aplicarCupom('DESCONTO10');
-var_dump($carrinho->obterResumo());
+print $err . "<br>";
+//var_dump($carrinho->obterResumo()) . "<br>";
 $carrinho->removerCupom();
-var_dump($carrinho->obterResumo());
+print $err . "<br>";
+//var_dump($carrinho->obterResumo()) . "<br>";
 
 ?>
